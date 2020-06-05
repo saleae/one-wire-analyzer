e71cb6ebacbddb4d761c1147438af27470f30cdb
Marcus10110 <markgarrison@saleae.com>
2020-06-05 15:35:54 -0700

    clang-format update of 1-Wire Analyzer

--- src/OneWireAnalyzerResults.h
+++ src/OneWireAnalyzerResults.h
@@ -11,22 +11,21 @@ class OneWireAnalyzerSettings;
 
 class OneWireAnalyzerResults : public AnalyzerResults
 {
-public:
-	OneWireAnalyzerResults( OneWireAnalyzer* analyzer, OneWireAnalyzerSettings* settings );
-	virtual ~OneWireAnalyzerResults();
+  public:
+    OneWireAnalyzerResults( OneWireAnalyzer* analyzer, OneWireAnalyzerSettings* settings );
+    virtual ~OneWireAnalyzerResults();
 
-	virtual void GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base );
-	virtual void GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id );
+    virtual void GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base );
+    virtual void GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id );
 
-	virtual void GenerateFrameTabularText(U64 frame_index, DisplayBase display_base );
-	virtual void GeneratePacketTabularText( U64 packet_id, DisplayBase display_base );
-	virtual void GenerateTransactionTabularText( U64 transaction_id, DisplayBase display_base );
+    virtual void GenerateFrameTabularText( U64 frame_index, DisplayBase display_base );
+    virtual void GeneratePacketTabularText( U64 packet_id, DisplayBase display_base );
+    virtual void GenerateTransactionTabularText( U64 transaction_id, DisplayBase display_base );
 
-protected: //functions
-
-protected:  //vars
-	OneWireAnalyzerSettings* mSettings;
-	OneWireAnalyzer* mAnalyzer;
+  protected: // functions
+  protected: // vars
+    OneWireAnalyzerSettings* mSettings;
+    OneWireAnalyzer* mAnalyzer;
 };
 
-#endif //ONE_WIRE_ANALYZER_RESULTS
+#endif // ONE_WIRE_ANALYZER_RESULTS
--- src/OneWireAnalyzerSettings.cpp
+++ src/OneWireAnalyzerSettings.cpp
@@ -4,29 +4,27 @@
 #include <sstream>
 #include <cstring>
 
-OneWireAnalyzerSettings::OneWireAnalyzerSettings()
-:	mOneWireChannel ( UNDEFINED_CHANNEL ),
-mOverdrive( false )
+OneWireAnalyzerSettings::OneWireAnalyzerSettings() : mOneWireChannel( UNDEFINED_CHANNEL ), mOverdrive( false )
 {
-	mOneWireChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mOneWireChannelInterface->SetTitleAndTooltip( "1-Wire", "Maxim 1-Wire Interface" );
-	mOneWireChannelInterface->SetChannel( mOneWireChannel );
+    mOneWireChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+    mOneWireChannelInterface->SetTitleAndTooltip( "1-Wire", "Maxim 1-Wire Interface" );
+    mOneWireChannelInterface->SetChannel( mOneWireChannel );
 
-	mOverDriveInterface.reset( new AnalyzerSettingInterfaceBool() );
-	mOverDriveInterface->SetTitleAndTooltip( "", "The analyzer will only operate at high speed overdrive mode." );
-	mOverDriveInterface->SetCheckBoxText( "Overdrive only mode" );
-	mOverDriveInterface->SetValue( false );
+    mOverDriveInterface.reset( new AnalyzerSettingInterfaceBool() );
+    mOverDriveInterface->SetTitleAndTooltip( "", "The analyzer will only operate at high speed overdrive mode." );
+    mOverDriveInterface->SetCheckBoxText( "Overdrive only mode" );
+    mOverDriveInterface->SetValue( false );
 
-	AddInterface( mOneWireChannelInterface.get() );
-	AddInterface( mOverDriveInterface.get() );
+    AddInterface( mOneWireChannelInterface.get() );
+    AddInterface( mOverDriveInterface.get() );
 
-	//AddExportOption( 0, "Export as text/csv file", "text (*.txt);;csv (*.csv)" );
-	AddExportOption( 0, "Export as text/csv file" );
-	AddExportExtension( 0, "text", "txt" );
-	AddExportExtension( 0, "csv", "csv" );
+    // AddExportOption( 0, "Export as text/csv file", "text (*.txt);;csv (*.csv)" );
+    AddExportOption( 0, "Export as text/csv file" );
+    AddExportExtension( 0, "text", "txt" );
+    AddExportExtension( 0, "csv", "csv" );
 
-	ClearChannels();
-	AddChannel( mOneWireChannel, "1-WIRE", false );
+    ClearChannels();
+    AddChannel( mOneWireChannel, "1-WIRE", false );
 }
 
 OneWireAnalyzerSettings::~OneWireAnalyzerSettings()
@@ -35,48 +33,47 @@ OneWireAnalyzerSettings::~OneWireAnalyzerSettings()
 
 void OneWireAnalyzerSettings::UpdateInterfacesFromSettings()
 {
-	mOneWireChannelInterface->SetChannel( mOneWireChannel );
-	mOverDriveInterface->SetValue( mOverdrive );
+    mOneWireChannelInterface->SetChannel( mOneWireChannel );
+    mOverDriveInterface->SetValue( mOverdrive );
 }
 
 bool OneWireAnalyzerSettings::SetSettingsFromInterfaces()
 {
-	mOneWireChannel = mOneWireChannelInterface->GetChannel();
-	mOverdrive = mOverDriveInterface->GetValue();
+    mOneWireChannel = mOneWireChannelInterface->GetChannel();
+    mOverdrive = mOverDriveInterface->GetValue();
 
-	ClearChannels();
-	AddChannel( mOneWireChannel, "1-WIRE", true );
+    ClearChannels();
+    AddChannel( mOneWireChannel, "1-WIRE", true );
 
-	return true;
+    return true;
 }
 
 void OneWireAnalyzerSettings::LoadSettings( const char* settings )
 {
-	SimpleArchive text_archive;
-	text_archive.SetString( settings );
+    SimpleArchive text_archive;
+    text_archive.SetString( settings );
 
-	const char* name_string;	//the first thing in the archive is the name of the protocol analyzer that the data belongs to.
-	text_archive >> &name_string;
-	if( strcmp( name_string, "SaleaeOneWireAnalyzer" ) != 0 )
-		AnalyzerHelpers::Assert( "SaleaeOneWireAnalyzer: Provided with a settings string that doesn't belong to us;" );
+    const char* name_string; // the first thing in the archive is the name of the protocol analyzer that the data belongs to.
+    text_archive >> &name_string;
+    if( strcmp( name_string, "SaleaeOneWireAnalyzer" ) != 0 )
+        AnalyzerHelpers::Assert( "SaleaeOneWireAnalyzer: Provided with a settings string that doesn't belong to us;" );
 
-	text_archive >>  mOneWireChannel;
-	text_archive >> mOverdrive;
+    text_archive >> mOneWireChannel;
+    text_archive >> mOverdrive;
 
-	ClearChannels();
-	AddChannel( mOneWireChannel, "1-WIRE", true );
+    ClearChannels();
+    AddChannel( mOneWireChannel, "1-WIRE", true );
 
-	UpdateInterfacesFromSettings();
+    UpdateInterfacesFromSettings();
 }
 
 const char* OneWireAnalyzerSettings::SaveSettings()
 {
-	SimpleArchive text_archive;
+    SimpleArchive text_archive;
 
-	text_archive << "SaleaeOneWireAnalyzer";
-	text_archive <<  mOneWireChannel;
-	text_archive << mOverdrive;
+    text_archive << "SaleaeOneWireAnalyzer";
+    text_archive << mOneWireChannel;
+    text_archive << mOverdrive;
 
-	return SetReturnString( text_archive.GetString() );
+    return SetReturnString( text_archive.GetString() );
 }
-
